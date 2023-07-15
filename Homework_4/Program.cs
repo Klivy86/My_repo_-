411085da96b8cac430fe802c50da7c0d0f13c286
Slava_Klimov <Vetslava86@gmail.com>
2023-07-15 22:15:24 +0300

    Added a solution to problem №27

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -17,3 +17,21 @@ for (int i = 0; i < B; i++)
 }
     Console.WriteLine("Результат:" + result);
 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int tempNumber = number;
+
+        while (tempNumber != 0)
+        {
+            int digit = tempNumber % 10;
+            sum += digit;
+            tempNumber /= 10;
+        }
+
+        Console.WriteLine("Сумма цифр числа: " + sum);

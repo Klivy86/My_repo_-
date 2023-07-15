d57402573053eb31fd200dfc9a6e5ccef8194461
Slava_Klimov <Vetslava86@gmail.com>
2023-07-15 17:03:44 +0300

    Added a solution to problem №23

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -37,17 +37,30 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите координаты точки A (x, y, z):");
-        double x1 = Convert.ToDouble(Console.ReadLine());
-        double y1 = Convert.ToDouble(Console.ReadLine());
-        double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки A (x, y, z):");
+//         double x1 = Convert.ToDouble(Console.ReadLine());
+//         double y1 = Convert.ToDouble(Console.ReadLine());
+//         double z1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Введите координаты точки B (x, y, z):");
-        double x2 = Convert.ToDouble(Console.ReadLine());
-        double y2 = Convert.ToDouble(Console.ReadLine());
-        double z2 = Convert.ToDouble(Console.ReadLine());
+//         Console.WriteLine("Введите координаты точки B (x, y, z):");
+//         double x2 = Convert.ToDouble(Console.ReadLine());
+//         double y2 = Convert.ToDouble(Console.ReadLine());
+//         double z2 = Convert.ToDouble(Console.ReadLine());
 
-        double distance = Math.Sqrt((x2 - x1)*(x2 - x1) + ((y2 - y1)*(y2 - y1)) + ((z2 - z1)*(z2 - z1)));
+//         double distance = Math.Sqrt((x2 - x1)*(x2 - x1) + ((y2 - y1)*(y2 - y1)) + ((z2 - z1)*(z2 - z1)));
 
- Console.WriteLine("Расстояние между точкой A и точкой B: " + distance.ToString("F2"));
-  
+//  Console.WriteLine("Расстояние между точкой A и точкой B: " + distance.ToString("F2"));
+
+//   Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел от 1 до " + N + ":");
+
+        for (int i = 1; i <= N; i++)
+        {
+            int cube = i * i * i;
+            Console.Write(cube + " ");
+        }

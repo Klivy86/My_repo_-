7740b1d5c531fe75df9774e9d0683cbccd577fd0
Slava_Klimov <Vetslava86@gmail.com>
2023-07-15 22:23:43 +0300

    Added a solution to problem №29

--- Homework_4/Program.cs
+++ Homework_4/Program.cs
@@ -3,35 +3,54 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите число B: ");
-int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
 
-int result = 1;
+// int result = 1;
 
-for (int i = 0; i < B; i++)
-{
-   result *= A;
-}
-    Console.WriteLine("Результат:" + result);
+// for (int i = 0; i < B; i++)
+// {
+//    result *= A;
+// }
+//     Console.WriteLine("Результат:" + result);
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
-Console.WriteLine("Введите число:");
-        int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число:");
+//         int number = Convert.ToInt32(Console.ReadLine());
 
-        int sum = 0;
-        int tempNumber = number;
+//         int sum = 0;
+//         int tempNumber = number;
 
-        while (tempNumber != 0)
+//         while (tempNumber != 0)
+//         {
+//             int digit = tempNumber % 10;
+//             sum += digit;
+//             tempNumber /= 10;
+//         }
+
+//         Console.WriteLine("Сумма цифр числа: " + sum);
+
+ 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] numbers = new int[8] { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+        Console.WriteLine("Элементы массива:");
+        Console.Write("[");
+        for (int i = 0; i < numbers.Length; i++)
         {
-            int digit = tempNumber % 10;
-            sum += digit;
-            tempNumber /= 10;
+            Console.Write(numbers[i]);
+            if (i < numbers.Length - 1)
+            {
+                Console.Write(", ");
+            }
         }
-
-        Console.WriteLine("Сумма цифр числа: " + sum);
+        Console.WriteLine("]");

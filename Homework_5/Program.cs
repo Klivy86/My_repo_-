c6bf3ba79928404103062f35e936906115963a5b
Slava_Klimov <Vetslava86@gmail.com>
2023-07-20 16:02:06 +0300

    Added problem solution №36

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -2,50 +2,91 @@
 // [345, 897, 568, 234] -> 2
 
   
-        int[] numbers = GenerateRandomArray(2, 100, 999);
+    //     int[] numbers = GenerateRandomArray(2, 100, 999);
 
-        Console.WriteLine("Массив чисел:");
-        PrintArray(numbers);
+    //     Console.WriteLine("Массив чисел:");
+    //     PrintArray(numbers);
 
-        int countEven = CountEvenNumbers(numbers);
-        Console.WriteLine("Количество четных чисел: " + countEven);
+    //     int countEven = CountEvenNumbers(numbers);
+    //     Console.WriteLine("Количество четных чисел: " + countEven);
     
 
-    static int[] GenerateRandomArray(int length, int minValue, int maxValue)
-    {
+    // static int[] GenerateRandomArray(int length, int minValue, int maxValue)
+    // {
+    //     Random random = new Random();
+    //     int[] array = new int[length];
+    //     for (int i = 0; i < length; i++)
+    //     {
+    //         array[i] = random.Next(minValue, maxValue + 1);
+    //     }
+    //     return array;
+    // }
+
+
+
+    // static void PrintArray(int[] array)
+    // {
+    //     Console.Write("[");
+    //     for (int i = 0; i < array.Length; i++)
+    //     {
+    //         Console.Write(array[i]);
+    //         if (i < array.Length - 1)
+    //         {
+    //             Console.Write(", ");
+    //         }
+    //     }
+    //     Console.WriteLine("]");
+    // }
+
+    // static int CountEvenNumbers(int[] array)
+    // {
+    //     int count = 0;
+    //     for (int i = 0; i < array.Length; i++)
+    //     {
+    //         if (array[i] % 2 == 0)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+
+        int[] array = new int[4]; // Размер массива можно изменить по вашему усмотрению
         Random random = new Random();
-        int[] array = new int[length];
-        for (int i = 0; i < length; i++)
-        {
-            array[i] = random.Next(minValue, maxValue + 1);
-        }
-        return array;
-    }
 
-    static void PrintArray(int[] array)
-    {
-        Console.Write("[");
+        // Заполняем массив случайными числами
         for (int i = 0; i < array.Length; i++)
         {
-            Console.Write(array[i]);
-            if (i < array.Length - 1)
-            {
-                Console.Write(", ");
-            }
+            array[i] = random.Next(1, 101); // Генерируем случайное число от 1 до 100
         }
-        Console.WriteLine("]");
-    }
 
-    static int CountEvenNumbers(int[] array)
+        // Находим сумму элементов на нечётных позициях
+        int result = SumOddPositions(array);
+
+        Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+        Console.WriteLine("Сумма элементов на нечётных позициях: " + result);
+    
+
+    static int SumOddPositions(int[] arr)
     {
-        int count = 0;
-        for (int i = 0; i < array.Length; i++)
+        int sum = 0;
+
+        // Обходим массив и складываем элементы на нечётных позициях
+        for (int i = 1; i < arr.Length; i += 2)
         {
-            if (array[i] % 2 == 0)
-            {
-                count++;
-            }
+            sum += arr[i];
         }
-        return count;
+
+        return sum;
     }
 
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76

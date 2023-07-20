5a1b82e0944cc922b6e44c6e13fc7a12cf58eb73
Slava_Klimov <Vetslava86@gmail.com>
2023-07-20 16:05:14 +0300

    Added problem solution №38

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -58,35 +58,59 @@
 
 
 
-        int[] array = new int[4]; // Размер массива можно изменить по вашему усмотрению
-        Random random = new Random();
+    //     int[] array = new int[4]; // Размер массива можно изменить по вашему усмотрению
+    //     Random random = new Random();
 
-        // Заполняем массив случайными числами
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = random.Next(1, 101); // Генерируем случайное число от 1 до 100
-        }
+    //     // Заполняем массив случайными числами
+    //     for (int i = 0; i < array.Length; i++)
+    //     {
+    //         array[i] = random.Next(1, 101); // Генерируем случайное число от 1 до 100
+    //     }
 
-        // Находим сумму элементов на нечётных позициях
-        int result = SumOddPositions(array);
+    //     // Находим сумму элементов на нечётных позициях
+    //     int result = SumOddPositions(array);
 
-        Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
-        Console.WriteLine("Сумма элементов на нечётных позициях: " + result);
+    //     Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+    //     Console.WriteLine("Сумма элементов на нечётных позициях: " + result);
     
 
-    static int SumOddPositions(int[] arr)
-    {
-        int sum = 0;
+    // static int SumOddPositions(int[] arr)
+    // {
+    //     int sum = 0;
 
-        // Обходим массив и складываем элементы на нечётных позициях
-        for (int i = 1; i < arr.Length; i += 2)
-        {
-            sum += arr[i];
-        }
+    //     // Обходим массив и складываем элементы на нечётных позициях
+    //     for (int i = 1; i < arr.Length; i += 2)
+    //     {
+    //         sum += arr[i];
+    //     }
 
-        return sum;
-    }
+    //     return sum;
+    // }
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+
+
+        double[] array = { 3.22, 4.2, 1.15, 77.15, 65.2 };
+        
+        // Находим максимальное и минимальное значения в массиве
+        double max = array[0];
+        double min = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+                max = array[i];
+
+            if (array[i] < min)
+                min = array[i];
+        }
+
+        // Вычисляем разницу между максимальным и минимальным элементами
+        double difference = max - min;
+
+        Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+        Console.WriteLine("Разница между максимальным и минимальным элементами: " + difference);

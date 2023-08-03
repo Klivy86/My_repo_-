c56cbe9f451c8447ef05fe7debc6723d090b6ae6
Slava_Klimov <Vetslava86@gmail.com>
2023-08-03 16:36:55 +0300

    Added problem solution №58

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -47,38 +47,101 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] matrix = {
-            { 1, 4, 7, 2 },
-            { 5, 9, 2, 3 },
-            { 8, 4, 2, 4 },
-            { 5, 2, 6, 7 }
+// int[,] matrix = {
+//             { 1, 4, 7, 2 },
+//             { 5, 9, 2, 3 },
+//             { 8, 4, 2, 4 },
+//             { 5, 2, 6, 7 }
+//         };
+
+// int minRow = FindRowWithMinSum(matrix);
+// {
+//     // Выводим результат
+//     Console.WriteLine("Номер строки с наименьшей суммой элементов: " + minRow);
+// }
+
+// static int FindRowWithMinSum(int[,] arr)
+// {
+//     int minSum = int.MaxValue;
+//     int minRow = -1;
+
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             sum += arr[i, j];
+//         }
+
+//         if (sum < minSum)
+//         {
+//             minSum = sum;
+//             minRow = i;
+//         }
+//     }
+
+//     return minRow;
+// }
+// Задача 58:  Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+
+int[,] matrix1 = {
+            { 2, 4 },
+            { 3, 2 }
         };
 
-int minRow = FindRowWithMinSum(matrix);
-{
-    // Выводим результат
-    Console.WriteLine("Номер строки с наименьшей суммой элементов: " + minRow);
-}
+int[,] matrix2 = {
+            { 3, 4 },
+            { 3, 3 }
+        };
 
-static int FindRowWithMinSum(int[,] arr)
-{
-    int minSum = int.MaxValue;
-    int minRow = -1;
+int[,] productMatrix = MultiplyMatrices(matrix1, matrix2);
 
-    for (int i = 0; i < arr.GetLength(0); i++)
+// Выводим результат
+Console.WriteLine("Произведение матриц:");
+PrintMatrix(productMatrix);
+    
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+{
+    int rows1 = matrix1.GetLength(0);
+    int cols1 = matrix1.GetLength(1);
+    int rows2 = matrix2.GetLength(0);
+    int cols2 = matrix2.GetLength(1);
+
+    if (cols1 != rows2)
+        throw new ArgumentException("Несовместимые матрицы для умножения.");
+
+    int[,] result = new int[rows1, cols2];
+
+    for (int i = 0; i < rows1; i++)
     {
-        int sum = 0;
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < cols2; j++)
         {
-            sum += arr[i, j];
-        }
-
-        if (sum < minSum)
-        {
-            minSum = sum;
-            minRow = i;
+            int sum = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            result[i, j] = sum;
         }
     }
 
-    return minRow;
+    return result;
+}
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
